3a833248f328fbba6a31241f214c1f5c081bb24f
Tatsuya Iwanaga <e165740@ie.u-ryukyu.ac.jp>
2016-12-09 16:33:01 +0900

    ステップ1

--- report5.java
+++ report5.java
@@ -2,6 +2,9 @@
  * Created by e165740 on 2016/12/06.
  */
 public class report5 {
-    String str = null;
-    System.out.printf("%d",str.length());
+    public static void main(String[] args) {
+        String str = null;
+        System.out.printf("%s", str.length());
+
+    }
 }
